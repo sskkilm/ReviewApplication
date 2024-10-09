8a505c28fddd6e2f36f3f080bcbddc44419e4b8a
sskkilm <sskkilm@naver.com>
2024-10-09 10:58:08 +0900

    init commit

---
+++ src/test/java/com/example/reviewapplication/ReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reviewapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
